24be1eb67dc0689caac0244939dfc1bebae6a65b
Seth Lawler <slawler@dewberry.com>
2025-01-22 21:27:52 -0500

    use points for decimal sep and get rid of default stac catalogs

--- src/locales/en/stac-fields.js
+++ src/locales/en/stac-fields.js
@@ -4,7 +4,7 @@ export default function run(/*locale*/) {
   const defaults = I18N.getDefaults();
   // We want the globally recommended number format with spaces as thousands separator
   // and a dot as decimal separator, which is available in e.g. fr-FR
-  I18N.numberFormatter = new Intl.NumberFormat("fr-FR", defaults.numberFormatterOptions);
+  I18N.numberFormatter = new Intl.NumberFormat("en-US", defaults.numberFormatterOptions);
   // We want ISO formatting for the date, which is available in e.g. sv-SE
   I18N.dateFormatter = new Intl.DateTimeFormat("sv-SE", defaults.dateFormatterOptions);
   // We want ISO formatting for the date and time, which is available in e.g. sv-SE
